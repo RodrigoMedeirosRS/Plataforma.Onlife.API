008fdc037b5329f96d832e5255407d9cc8e2f098
R.M.Lehnemann <rodrigo.medeiros.email@gmail.com.br>
2022-11-02 13:35:05 -0300

    feat(ajustes banco dedos)

--- DOC/Estrutura Banco/CreateScript.sql
+++ DOC/Estrutura Banco/CreateScript.sql
@@ -1,10 +1,3 @@
-CREATE TABLE Pessoa (
-  Codigo SERIAL  NOT NULL ,
-  Nome VARCHAR(30)   NOT NULL ,
-  Sobrenome VARCHAR(50)   NOT NULL ,
-  Genero VARCHAR(20)   NOT NULL   ,
-PRIMARY KEY(Codigo));
-
 CREATE TABLE TipoRelacao (
   Codigo SERIAL  NOT NULL ,
   Nome VARCHAR(50) UNIQUE NOT NULL   ,
@@ -12,30 +5,38 @@ PRIMARY KEY(Codigo));
 
 CREATE TABLE TipoDeExecucao (
   Codigo SERIAL  NOT NULL ,
-  Nome VARCHAR(40) UNIQUE NOT NULL   ,
+  Nome VARCHAR(40) UNIQUE NOT NULL ,
   Binario BOOL   NOT NULL   ,
 PRIMARY KEY(Codigo));
 
-CREATE TABLE LocalizacaoGeografica (
+CREATE TABLE Pessoa (
   Codigo SERIAL  NOT NULL ,
-  Latitude DOUBLE PRECISION   NOT NULL ,
-  Longitude DOUBLE PRECISION   NOT NULL   ,
-PRIMARY KEY(Codigo));
-
-CREATE TABLE Apelido (
-  Codigo SERIAL  NOT NULL ,
-  Nome VARCHAR(20)   NOT NULL   ,
+  Nome VARCHAR(30) UNIQUE NOT NULL ,
+  Apelido VARCHAR(50)    ,
+  Foto TEXT    ,
+  ResearchGate VARCHAR(300)    ,
+  Linkedin VARCHAR(300)    ,
+  Lattes VARCHAR(300)      ,
 PRIMARY KEY(Codigo));
 
 CREATE TABLE Idioma (
   Codigo SERIAL  NOT NULL ,
-  Nome VARCHAR(20)  UNIQUE NOT NULL ,
+  Nome VARCHAR(20) UNIQUE NOT NULL,
+PRIMARY KEY(Codigo));
+
+CREATE TABLE Localidade (
+  Codigo SERIAL  NOT NULL ,
+  Nome VARCHAR(200) UNIQUE  NOT NULL ,
+  Descricao VARCHAR(500)   NOT NULL ,
+  Mapa TEXT   NOT NULL ,
+  Latitude INTEGER   NOT NULL ,
+  Longitude INTEGER   NOT NULL   ,
 PRIMARY KEY(Codigo));
 
 CREATE TABLE Tipo (
   Codigo SERIAL  NOT NULL ,
   TipoDeExecucao INTEGER   NOT NULL ,
-  Nome VARCHAR(30)  UNIQUE NOT NULL ,
+  Nome VARCHAR(30) UNIQUE   NOT NULL ,
   Extensao VARCHAR(7)   NOT NULL   ,
 PRIMARY KEY(Codigo)  ,
   FOREIGN KEY(TipoDeExecucao)
@@ -45,29 +46,17 @@ CREATE INDEX Tipo_FKIndex1 ON Tipo (TipoDeExecucao);
 
 CREATE INDEX IFK_Rel_27 ON Tipo (TipoDeExecucao);
 
-CREATE TABLE PessoaApelido (
-  Codigo SERIAL  NOT NULL ,
-  Apelido INTEGER   NOT NULL ,
-  Pessoa INTEGER   NOT NULL   ,
-PRIMARY KEY(Codigo)    ,
-  FOREIGN KEY(Pessoa)
-    REFERENCES Pessoa(Codigo),
-  FOREIGN KEY(Apelido)
-    REFERENCES Apelido(Codigo));
-
-CREATE INDEX PessoaApelido_FKIndex1 ON PessoaApelido (Pessoa);
-CREATE INDEX PessoaApelido_FKIndex2 ON PessoaApelido (Apelido);
-
-CREATE INDEX IFK_Rel_07 ON PessoaApelido (Pessoa);
-CREATE INDEX IFK_Rel_08 ON PessoaApelido (Apelido);
-
 CREATE TABLE Registro (
   Codigo SERIAL  NOT NULL ,
   Idioma INTEGER   NOT NULL ,
   Tipo INTEGER   NOT NULL ,
-  Nome VARCHAR(30) UNIQUE  NOT NULL ,
+  Nome VARCHAR(30) UNIQUE NOT NULL ,
+  Apelido VARCHAR(30)    ,
   Conteudo TEXT   NOT NULL ,
-  DataInsercao TIMESTAMP   NOT NULL   ,
+  DataInsercao TIMESTAMP   NOT NULL ,
+  Latitude INTEGER    ,
+  Longitude INTEGER    ,
+  Descricao VARCHAR(500)      ,
 PRIMARY KEY(Codigo)    ,
   FOREIGN KEY(Tipo)
     REFERENCES Tipo(Codigo),
@@ -101,49 +90,21 @@ CREATE INDEX IFK_Rel_09 ON PessoaRegistro (Pessoa);
 CREATE INDEX IFK_Rel_10 ON PessoaRegistro (Registro);
 CREATE INDEX IFK_Rel_11 ON PessoaRegistro (TipoRelacao);
 
-CREATE TABLE Descricao (
+CREATE TABLE RegistroLocalidade (
   Codigo SERIAL  NOT NULL ,
-  Registro INTEGER   NOT NULL ,
-  Conteudo TEXT   NOT NULL   ,
-PRIMARY KEY(Codigo)  ,
-  FOREIGN KEY(Registro)
-    REFERENCES Registro(Codigo));
-
-CREATE INDEX Descricao_FKIndex1 ON Descricao (Registro);
-
-CREATE INDEX IFK_Rel_14 ON Descricao (Registro);
-
-CREATE TABLE RegistroLocalizacao (
-  Codigo SERIAL  NOT NULL ,
-  Registro INTEGER   NOT NULL ,
-  LocalizacaoGeografica INTEGER   NOT NULL   ,
+  Localidade INTEGER   NOT NULL ,
+  Registro INTEGER   NOT NULL   ,
 PRIMARY KEY(Codigo)    ,
-  FOREIGN KEY(LocalizacaoGeografica)
-    REFERENCES LocalizacaoGeografica(Codigo),
   FOREIGN KEY(Registro)
-    REFERENCES Registro(Codigo));
+    REFERENCES Registro(Codigo),
+  FOREIGN KEY(Localidade)
+    REFERENCES Localidade(Codigo));
 
-CREATE INDEX RegistroLocalizacao_FKIndex1 ON RegistroLocalizacao (LocalizacaoGeografica);
-CREATE INDEX RegistroLocalizacao_FKIndex2 ON RegistroLocalizacao (Registro);
+CREATE INDEX RegistroLocalidade_FKIndex1 ON RegistroLocalidade (Registro);
+CREATE INDEX RegistroLocalidade_FKIndex2 ON RegistroLocalidade (Localidade);
 
-CREATE INDEX IFK_Rel_21 ON RegistroLocalizacao (LocalizacaoGeografica);
-CREATE INDEX IFK_Rel_22 ON RegistroLocalizacao (Registro);
-
-CREATE TABLE RegistroApelido (
-  Codigo SERIAL  NOT NULL ,
-  Registro INTEGER   NOT NULL ,
-  Apelido INTEGER   NOT NULL   ,
-PRIMARY KEY(Codigo)    ,
-  FOREIGN KEY(Apelido)
-    REFERENCES Apelido(Codigo),
-  FOREIGN KEY(Registro)
-    REFERENCES Registro(Codigo));
-
-CREATE INDEX RegistroApelido_FKIndex1 ON RegistroApelido (Apelido);
-CREATE INDEX RegistroApelido_FKIndex2 ON RegistroApelido (Registro);
-
-CREATE INDEX IFK_Rel_12 ON RegistroApelido (Apelido);
-CREATE INDEX IFK_Rel_13 ON RegistroApelido (Registro);
+CREATE INDEX IFK_Rel_19 ON RegistroLocalidade (Registro);
+CREATE INDEX IFK_Rel_20 ON RegistroLocalidade (Localidade);
 
 CREATE TABLE Referencia (
   Codigo SERIAL  NOT NULL ,
--- DOC/Estrutura Banco/DropScript.sql
+++ DOC/Estrutura Banco/DropScript.sql
@@ -1,27 +1,21 @@
 DROP TABLE Referencia;
 
-DROP TABLE RegistroApelido;
-
-DROP TABLE RegistroLocalizacao;
-
-DROP TABLE Descricao;
+DROP TABLE RegistroLocalidade;
 
 DROP TABLE PessoaRegistro;
 
 DROP TABLE Registro;
 
-DROP TABLE PessoaApelido;
-
 DROP TABLE Tipo;
+
+DROP TABLE Localidade;
 
 DROP TABLE Idioma;
 
-DROP TABLE Apelido;
-
-DROP TABLE LocalizacaoGeografica;
+DROP TABLE Pessoa;
 
 DROP TABLE TipoDeExecucao;
 
 DROP TABLE TipoRelacao;
 
-DROP TABLE Pessoa;
+
